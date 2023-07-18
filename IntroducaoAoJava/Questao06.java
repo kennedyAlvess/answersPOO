4b0c14231e6b80cda9e6b0542823a5e97d96793e
KennedyAlvess <kenedy199717@gmail.com>
2023-07-18 19:48:15 -0300

    rsp questao 6

---
+++ IntroducaoAoJava/Questao06.java
@@ -0,0 +1,20 @@
+/*Escreva um programa que recebe um valor inteiro representando um intervalo em minutos
+e imprime o equivalente a esse período expresso em valores inteiros para dias, horas e minutos.
+Ex.: 9257 minutos = 6 dias, 10 horas e 17 minutos.*/
+package IntroducaoAoJava;
+
+import java.util.Scanner;
+
+public class Questao06 {
+    public static void main(String[] args) {
+        int numMinutos, totalDias, totalHoras, totalMinutos;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Informe o valor em minutos: ");
+        numMinutos = Integer.parseInt(input.nextLine());
+        input.close();
+        totalDias = numMinutos/1440;
+        totalHoras = (numMinutos%1440)/60;
+        totalMinutos = (numMinutos%1440)%60;
+        System.out.println(numMinutos+" minutos = "+totalDias+" dias,"+totalHoras+" horas e "+totalMinutos+" minutos.");
+    }
+}
