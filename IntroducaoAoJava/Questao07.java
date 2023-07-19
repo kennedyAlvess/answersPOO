7dcce127c2d6d1283d1ebd1ea5b174c1789c4d8f
KennedyAlvess <kenedy199717@gmail.com>
2023-07-18 21:11:21 -0300

    rsp questao 7

--- IntroducaoAoJava/Questao07.java
+++ IntroducaoAoJava/Questao07.java
@@ -5,18 +5,22 @@ import java.util.Scanner;
 
 public class Questao07 {
     public static void main(String[] args) {
-        double num, menor = 0;
+        double num1, num2, num3; //, menor = 0;
         Scanner input = new Scanner(System.in);
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Informe o "+(i+1)+" º número:");
-            num = input.nextDouble();
-            if (i == 0){
-                menor = num;
-            }else if( num < menor){
-                menor = num;
-            }
-        }
-        System.out.println("O menor número é: "+menor);
+//        for (int i = 0; i < 3; i++) {
+//            System.out.println("Informe o "+(i+1)+" º número:");
+//            num = input.nextDouble();
+//            if (i == 0){
+//                menor = num;
+//            }else if( num < menor){
+//                menor = num;
+//            }
+//        }
+        System.out.println("Informe três números: ");
+        num1 = input.nextDouble();
+        num2 = input.nextDouble();
+        num3 = input.nextDouble();
+        System.out.println("O menor número é: "+Math.min(Math.min(num1,num2),num3));
 
         input.close();
     }
