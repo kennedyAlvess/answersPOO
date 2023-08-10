cfd2df5f41be5a2fb0d111bd19c6a8d9ff264926
KennedyAlvess <kenedy199717@gmail.com>
2023-08-10 09:14:17 -0300

    rsp*

---
+++ IntroducaoParadigmasPOO/Questao26.java
@@ -0,0 +1,13 @@
+package IntroducaoParadigmasPOO;
+
+public class Questao26 {
+    private double a;
+    private double b;
+    private double c;
+
+    public void calcularDelta(double a, double b, double c){};
+    public void calcularX1(double a, double b, double delta){};
+    public void calcularX2(double a, double b, double delta){};
+
+
+}
