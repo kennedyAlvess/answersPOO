60a31a40429d6e0b21795ebb64e1de273cbfeeb7
KennedyAlvess <kenedy199717@gmail.com>
2023-08-10 09:03:24 -0300

    rsp*

--- IntroducaoParadigmasPOO/Questao23.java
+++ IntroducaoParadigmasPOO/Questao23.java
@@ -2,55 +2,11 @@ package IntroducaoParadigmasPOO;
 
 public class Questao23 {
     private double valor;
-    private double potencia;
-    private String tipo;
+    private int voltz;
     private String marca;
+    private String tipo;
 
-    public Questao23(double valor, double potencia, String tipo, String marca) {
-        this.valor = valor;
-        this.potencia = potencia;
-        this.tipo = tipo;
-        this.marca = marca;
-    }
+    public void vender(double valor){};
+    public void testar(){};
 
-    public double getValor() {
-        return valor;
-    }
-
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
-
-    public double getPotencia() {
-        return potencia;
-    }
-
-    public void setPotencia(double potencia) {
-        this.potencia = potencia;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    @Override
-    public String toString() {
-        return "Info. Lampada:\n" +
-                "valor = " + valor +
-                " R$ Reais\nPotência = " + potencia +
-                "\nTipo = " + tipo +
-                "\nMarca='" + marca ;
-    }
 }
