6aa6e68cf0417d2ed3b0b25d97f0e9f28415a2c9
KennedyAlvess <kenedy199717@gmail.com>
2023-08-14 21:46:40 -0300

    rsp*

---
+++ CriacaoDeClasses/Questao29/Fatura.java
@@ -0,0 +1,20 @@
+package CriacaoDeClasses.Questao29;
+
+public class Fatura {
+    private int numeroIdentificador;
+    private String descricaoDoProduto;
+    private int quantidadeComprada;
+    private double valorUnitario;
+
+    public Fatura(int numeroIdentificador, String descricaoDoProduto, int quantidadeComprada, double valorUnitario) {
+        if (quantidadeComprada < 0) this.quantidadeComprada = 0 ; else {this.quantidadeComprada = quantidadeComprada;}
+        if (valorUnitario < 0) this.valorUnitario = 0 ; else {this.valorUnitario = valorUnitario;}
+        this.numeroIdentificador = numeroIdentificador;
+        this.descricaoDoProduto = descricaoDoProduto;
+    }
+
+    public double calcularTotal(){
+        return quantidadeComprada*valorUnitario;
+    }
+
+}
