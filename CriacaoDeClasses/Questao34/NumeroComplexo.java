f3ebc08b83c29a71ba6ef9ffade7d293faff759d
KennedyAlvess <kenedy199717@gmail.com>
2023-08-22 00:37:45 -0300

    rsp*

---
+++ CriacaoDeClasses/Questao34/NumeroComplexo.java
@@ -0,0 +1,25 @@
+package CriacaoDeClasses.Questao34;
+
+public class NumeroComplexo {
+    private double numeroReal;
+    private double numeroImaginario;
+
+    public NumeroComplexo(double numeroReal, double numeroImaginario) {
+        this.numeroReal = numeroReal;
+        this.numeroImaginario = numeroImaginario;
+    }
+    public NumeroComplexo(double numeroReal) {
+        this.numeroReal = numeroReal;
+        this.numeroImaginario = 0;
+    }
+    public NumeroComplexo() {
+        this.numeroReal = 0;
+        this.numeroImaginario = 0;
+    }
+    @Override
+    public String toString() {
+        return (this.numeroImaginario < 0)? "Número complexo: "+this.numeroReal+this.numeroImaginario+"i" :
+                "Número complexo: "+this.numeroReal+"+"+this.numeroImaginario+"i";
+    }
+}
+
