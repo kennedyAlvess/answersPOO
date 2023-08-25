4cd2ec31cead8e266f01adf5ac287bdec62eb23f
KennedyAlvess <kenedy199717@gmail.com>
2023-08-25 00:53:35 -0300

    rsp*

---
+++ CriacaoDeClasses/Questao36/Main.java
@@ -0,0 +1,17 @@
+package CriacaoDeClasses.Questao36;
+
+public class Main {
+    public static void main(String[] args) {
+        RegistroAcademico registro1 = new RegistroAcademico();
+        RegistroAcademico registro2 = new RegistroAcademico();
+        RegistroAcademico registro3 = new RegistroAcademico();
+
+        registro1.inicializaRegistroAcademico("Maria",111,0.50);
+        registro2.inicializaRegistroAcademico("Joao",222,0.60);
+        registro3.inicializaRegistroAcademico("Chico",333,0.70);
+
+        System.out.println(registro1);
+        System.out.println(registro2);
+        System.out.println(registro3);
+    }
+}
