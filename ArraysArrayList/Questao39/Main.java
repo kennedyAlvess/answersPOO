641f89859939d3da13e7e39489f84b6a9a0f267f
KennedyAlvess <kenedy199717@gmail.com>
2023-09-06 17:53:16 -0300

    rsp*

---
+++ ArraysArrayList/Questao39/Main.java
@@ -0,0 +1,10 @@
+package ArraysArrayList.Questao39;
+
+public class Main {
+    public static void main(String[] args) {
+        Matriz matriz1 = new Matriz();
+
+        System.out.println("Determinante da matriz: "+matriz1.calcularDeterminante());
+        matriz1.imprimirMatriz();
+    }
+}
