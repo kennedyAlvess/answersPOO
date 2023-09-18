9de5ca836334229cbf856b5fabf4b51b51bf7c2d
KennedyAlvess <kenedy199717@gmail.com>
2023-09-18 19:19:35 -0300

    rsp*

--- Provas/SegundaUnidade/P_2023_1_v2/Questao04/Main.java
+++ Provas/SegundaUnidade/P_2023_1_v2/Questao04/Main.java
@@ -7,6 +7,17 @@ public class Main {
         String entrada = "HELLO THERE";
 
         TreeMap<Character, Integer> ocorrencias = new TreeMap<>();
-        
+
+        for (int i = 0; i < entrada.length(); i++) {
+            Character key = entrada.charAt(i);
+            if (ocorrencias.containsKey(key)) {
+                int value = ocorrencias.get(key);
+                ocorrencias.put(key, ++value);
+            } else {
+                if (!key.equals(' ')) ocorrencias.put(key, 1);
+            }
+        }
+        System.out.println(ocorrencias);
+
     }
 }
