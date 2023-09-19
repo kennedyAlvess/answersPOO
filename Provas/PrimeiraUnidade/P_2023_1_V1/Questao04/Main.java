3d4478db072a3d31967b64dbb2d79d1f35eb650e
KennedyAlvess <kenedy199717@gmail.com>
2023-09-19 17:07:26 -0300

    rsp*

---
+++ Provas/PrimeiraUnidade/P_2023_1_V1/Questao04/Main.java
@@ -0,0 +1,12 @@
+package Provas.PrimeiraUnidade.P_2023_1_V1.Questao04;
+
+public class Main {
+    public static void main(String[] args) {
+        NumeroComplexo num1 = new NumeroComplexo(2.1,-5.2);
+        NumeroComplexo num2 = new NumeroComplexo(7);
+        NumeroComplexo num3 = new NumeroComplexo();
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+}
